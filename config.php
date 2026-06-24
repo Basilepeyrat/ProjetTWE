8ae38ceb6d2170d83c1cba39fb4270db1aa906a1
Basilepeyrat <basile.peyrat@gmail.com>
2026-06-24 14:06:57 +0200

    Retire config.php du suivi Git

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$BDD_host="localhost";
-$BDD_user="admin"; // meme que phpmyadmin
-$BDD_password="motdepasse"; // meme que phpmyadmin
-$BDD_base="ProjetTWE"; // nom de la base de données
-?>
